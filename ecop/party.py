01fb52cb2abef82a56a54f70f82fe6adf47babeb
Hong Yuan <hongyuan@homemaster.cn>
2017-08-24 12:30:54 +0800

    Allow CustomerPicker to search for partyId

--- ecop/party.py
+++ ecop/party.py
@@ -17,15 +17,25 @@ class PartyJSON(RpcBase):
 
     @jsonrpc_method(endpoint='rpc', method='party.search')
     def searchParty(self, query=None, partyId=None):
+        q = self.sess.query(Party)
         if partyId:
-            users = [self.sess.query(Party).get(partyId)]
+            users = [q.get(partyId)]
         elif query:
+            users = []
+
+            # if a 8 digit number is given ,try treat it as party id first
+            if len(query) == 8 and query.isdigit():
+                user = q.get(int(query))
+                if user:
+                    users.append(user)
+
             cond = or_(
                 Party.partyName.op('ilike')('%%%s%%' % query),
                 Party.login.op('ilike')('%%%s%%' % query))
             if query.isdigit():
                 cond = or_(cond, Party.mobile.op('ilike')('%%%s%%' % query))
-            users = self.sess.query(Party).filter(cond).limit(20).all()
+
+            users.extend(q.filter(cond).limit(20).all())
         else:
             # it is possible for the CustomerPicker store to send query without
             # the query value
