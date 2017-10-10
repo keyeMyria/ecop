9cde4fb50a3b32b2f8bc176a160d3f651f4569c7
Hong Yuan <hongyuan@homemaster.cn>
2017-10-10 14:55:52 +0800

    Upgrade elasticsearch to 5.x

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ requires = [
     'pyramid_rpc',  # for json rpc server
 
     'beautifulsoup4',
+    'elasticsearch-dsl>=5.0.0,<6.0.0',
     'genshi',
     'hiredis',
     'oss2',  # for Aliyun oss bucket management
