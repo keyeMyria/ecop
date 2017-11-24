a3600198a9d600651beba2faef359e932f2d99f7
Hong Yuan <hongyuan@homemaster.cn>
2017-11-24 17:39:23 +0800

    Allow inserting appointment button to page

--- client/src/widget/CKEditor.js
+++ client/src/widget/CKEditor.js
@@ -4,10 +4,10 @@ Ext.define('Ecop.widget.CKEditor', {
 
   config: {
     /*
-         * In order to keep the size of ckeditor in sync with the widget, we
-         * have to use the component layout logic and not pure css. So set
-         * this to false and add a listener to resize event.
-         */
+     * In order to keep the size of ckeditor in sync with the widget, we
+     * have to use the component layout logic and not pure css. So set
+     * this to false and add a listener to resize event.
+     */
     liquidLayout: false
   },
 
@@ -17,10 +17,10 @@ Ext.define('Ecop.widget.CKEditor', {
     me.editor = CKEDITOR.replace(me.inputEl.dom, {
       filebrowserUploadUrl: '/upload',
       language: 'zh-cn',
-      extraAllowedContent:
-        'map[!name]; area[title,!href,shape,coords]; img[usemap]',
+      extraAllowedContent: '*[class,id]; div(*); span; button[onclick]',
       removeButtons: 'h4,h5,h6,Strike'
     })
+
     // the delay is necessary so that when the widget is first intialized
     // the ckeditor will have enough time to be fully initialized so that
     // editor.resize method can be used
