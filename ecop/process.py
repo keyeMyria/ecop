7bcce735cc2214a9404301e58287799011cb70d4
Hong Yuan <hongyuan@homemaster.cn>
2018-03-22 18:14:30 +0800

    Method 'bpmn.process.start' is working with StartForm

--- ecop/process.py
+++ ecop/process.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from pyramid_rpc.jsonrpc import jsonrpc_method
 
 from weblibs.camunda import camundaClient as cc
-from weblibs.jsonrpc import RPCUserError
+from weblibs.jsonrpc import RPCUserError, parseDate
 
 from webmodel.consts import ORDER_SOURCE
 from webmodel.order import SalesOrder
@@ -13,21 +12,18 @@ from ecop.base import RpcBase
 class PorcessJSON(RpcBase):
     @jsonrpc_method(endpoint='rpc', method='bpmn.process.start')
     def startProcess(self, params):
-        externalOrderId = params['orderId']
+        params = parseDate(params,
+            fields=['scheduledMeasureDate', 'scheduledInstallDate'])
+
+        externalOrderId = params.pop('externalOrderId')
         if self.sess.query(SalesOrder).filter_by(
             orderSource=ORDER_SOURCE.IKEA,
             externalOrderId=externalOrderId
         ).all():
             raise RPCUserError('该订单号已存在，不能重复提交')
 
-        return
-
         cc.makeRequest('/process-definition/key/worktop/start', 'post', {
-            'businessKey': '124812412',
-            'variables': {
-                'scheduledMeasureDate': {
-                    'value': datetime.now().isoformat(),
-                    'type': 'Date'
-                }
-            }
-        })
+            'businessKey': externalOrderId,
+            },
+            variables=params
+        )
