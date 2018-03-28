161361193828e5a9c7a8731f65fc96e7685db3c4
Hong Yuan <hongyuan@homemaster.cn>
2018-03-28 12:05:10 +0800

    Implement TakeMeasurement form

--- worktop/src/tasks/TakeMeasurement.js
+++ worktop/src/tasks/TakeMeasurement.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import validation from 'react-validation-mixin'
 
 import { strategy, ValidatedForm, Field } from 'form'
@@ -30,17 +30,32 @@ class TakeMeasurement extends ValidatedForm {
 
   render = () => {
     const { values } = this.state
+    const { variables } = this.props
 
     return (
-      <Field
-        component={FileUploader}
-        name="measurementFile"
-        label="测量文件"
-        error={!!this.getFieldError('measurementFile')}
-        helperText={this.getFieldError('measurementFile')}
-        onChange={this.handleChange('measurementFile')}
-        value={values.measurementFile}
-      />
+      <Fragment>
+        <FileUploader
+          label="订单文件"
+          fullWidth
+          margin="normal"
+          allowUpload={false}
+          allowDelete={false}
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={variables.orderFile}
+        />
+
+        <Field
+          component={FileUploader}
+          name="measurementFile"
+          label="测量文件"
+          error={!!this.getFieldError('measurementFile')}
+          helperText={this.getFieldError('measurementFile')}
+          onChange={this.handleChange('measurementFile')}
+          value={values.measurementFile}
+        />
+      </Fragment>
     )
   }
 }
