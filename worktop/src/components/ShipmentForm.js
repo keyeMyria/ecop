82e95e807224a69e392e832f467adc96d7fbee81
Hong Yuan <hongyuan@homemaster.cn>
2018-03-29 12:28:06 +0800

    Implement the UI for ShipmentForm

--- worktop/src/components/ShipmentForm.js
+++ worktop/src/components/ShipmentForm.js
@@ -30,26 +30,72 @@ const styles = {
 }
 
 class ShipmentForm extends Component {
-  state = { orderIdList: [] }
+  state = { orderIds: '', errorMessage: '' }
 
-  handleSubmit = () => {}
+  handleSubmit = () => {
+    const value = this.state.orderIds
+    const lines = value.split('\n')
+
+    // any non-empty line must be 8 or 9 characters
+    for (let i = 0; i < lines.length; i++) {
+      let l = lines[i].length
+      if (l && l < 8) {
+        this.setState({ errorMessage: '订单号长度错误' })
+        return
+      }
+    }
+
+    this.setState({ errorMessage: '' })
+    console.log('Submittig form ')
+  }
+
+  handleChange = e => {
+    const { value } = e.target
+    const char = value.charAt(value.length - 1)
+
+    // only number and enter
+    if (char && char !== '\n' && (char < '0' || char > '9')) return
+
+    const lines = value.split('\n')
+    // Do not allow any line to exceed 9 characters
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].length > 9) return
+    }
+
+    /**
+     * When enter is pressed at the end, check that any **previous** line must
+     * be at least 8 long
+     */
+    if (char === '\n') {
+      for (let i = 0; i < lines.length - 1; i++) {
+        if (lines[i].length < 8) return
+      }
+    }
+
+    this.setState({ orderIds: value.replace('\n\n', '\n') })
+  }
 
   render = () => {
     const { classes } = this.props
-    const { values } = this.state
 
     return (
       <Paper className={classes.root}>
         <TextField
-          required={false}
-          autoFocus
-          InputProps={{ classes: { input: classes.orderId } }}
-          onChange={e => {
-            var { value } = e.target
-            if (/^\d{0,9}$/.test(value) || !value) {
-              this.setState({ values: { ...values, externalOrderId: value } })
-            }
+          required
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
           }}
+          label="发货订单号"
+          autoFocus
+          rows={20}
+          multiline
+          value={this.state.orderIds}
+          InputProps={{ classes: { input: classes.orderId } }}
+          onChange={this.handleChange}
+          error={!!this.state.errorMessage}
+          helperText={this.state.errorMessage}
         />
 
         <div className={classes.buttonRow}>
