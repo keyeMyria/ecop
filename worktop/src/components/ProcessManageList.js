78208da525e43c3f2f63569a6be8913738f44eb9
Hong Yuan <hongyuan@homemaster.cn>
2018-04-02 15:35:59 +0800

    Implement a mock process list

--- worktop/src/components/ProcessManageList.js
+++ worktop/src/components/ProcessManageList.js
@@ -4,14 +4,86 @@ import { connect } from 'react-redux'
 import compose from 'recompose/compose'
 
 import { withStyles } from 'material-ui/styles'
-
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel
+} from 'material-ui/Table'
 import { searchProcess } from 'model/actions'
 
 const styles = {
   root: {}
 }
 
+const columns = [
+  {
+    id: 'externalOrderId',
+    numeric: false,
+    disablePadding: true,
+    label: '订单号'
+  },
+  { id: 'startTime', numeric: true, disablePadding: false, label: '开始时间' },
+  { id: 'endTime', numeric: true, disablePadding: false, label: '完成时间' },
+  { id: 'status', numeric: true, disablePadding: false, label: '状态' }
+]
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property)
+  }
+
+  render() {
+    const { order, orderBy } = this.props
+
+    return (
+      <TableHead>
+        <TableRow>
+          {columns.map(column => {
+            return (
+              <TableCell
+                key={column.id}
+                numeric={column.numeric}
+                padding={column.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === column.id ? order : false}
+              >
+                <TableSortLabel
+                  active={orderBy === column.id}
+                  direction={order}
+                  onClick={this.createSortHandler(column.id)}
+                >
+                  {column.label}
+                </TableSortLabel>
+              </TableCell>
+            )
+          }, this)}
+        </TableRow>
+      </TableHead>
+    )
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired
+}
+
 class ProcessManageList extends Component {
+  state = {
+    data: [],
+    order: 'asc',
+    orderBy: 'startTime'
+  }
+
+  componentWillReceiveProps = nextProps => {
+    // make a copy of the process data
+    if (this.props.processes !== nextProps.processese) {
+      this.setState({ data: nextProps.processes.slice() })
+    }
+  }
+
   componentDidMount = () => {
     this.refresh()
   }
@@ -20,12 +92,30 @@ class ProcessManageList extends Component {
     this.props.dispatch(searchProcess())
   }
 
+  handleRequestSort = () => {}
+
   render() {
-    const { processes } = this.props
-    console.log(this.props)
+    const { classes } = this.props
+    const { data, order, orderBy } = this.state
 
     return (
-      <div>Hallo this is a list of {processes.length} items</div>
+      <Table className={classes.table}>
+        <EnhancedTableHead
+          order={order}
+          orderBy={orderBy}
+          onRequestSort={this.handleRequestSort}
+        />
+        <TableBody>
+          {data.map(n => (
+            <TableRow hover role="checkbox" tabIndex={-1} key={n.id}>
+              <TableCell padding="none">{n.businessKey}</TableCell>
+              <TableCell>{n.startTime}</TableCell>
+              <TableCell>{n.endTime}</TableCell>
+              <TableCell numeric>{n.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     )
   }
 }
